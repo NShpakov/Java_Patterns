e10eaf4d67c6873fccfe1c410220c2a9751f0843
Shpakov Nikolay <Shpakov.NN@ya.ru>
2021-12-27 23:04:51 +0300

    added builder

---
+++ src/creational/prototype/VersionControlRunner.java
@@ -0,0 +1,14 @@
+package creational.prototype;
+
+public class VersionControlRunner {
+	public static void main(String[] args) {
+		Project master = new Project(1, "SuperProject", "SourceCode sourceCode = new SourceCode;\n");
+		System.out.println(master);
+		// Project masterClone = (Project) master.copy();
+
+		ProjectFactory factory = new ProjectFactory(master);
+		Project masterClone = factory.cloneProject();
+		System.out.println("________________________________");
+		System.out.println(masterClone);
+	}
+}
